83c8707295e8c3b601f41c302fb47a6070a5aa26
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-16 14:18:53 +0300

    Update startConfig.sh

--- startConfig.sh
+++ startConfig.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-installerDir="./installer"
-echo "rise for <$installerDir> EXECUTABLE flag"
-chmod +x $installerDir/*
-
 userName="$1"
 userEmail="$1@inbox.ru"
 echo "basic config for Git: $userName <$userEmail>"
 git config --global user.name $userName
 git config --global user.email $userEmail
 echo
+
+installerDir="./installer"
+echo "rise for <$installerDir> EXECUTABLE flag"
+chmod +x $installerDir/*
+
