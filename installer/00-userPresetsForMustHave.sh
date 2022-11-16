9fd97dc4906c65c901448bb496f64bd61f69127f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-16 17:48:36 +0300

    Update 00-userPresetsForMustHave.sh

--- installer/00-userPresetsForMustHave.sh
+++ installer/00-userPresetsForMustHave.sh
@@ -4,7 +4,7 @@ echo add presets to user directory
 deployerRoot=$(dirname $(dirname $0))
 sourceUserDir=/resources/userDir/
 itemsList= .zshrc .vimrc
-for anItem in itemsList
+for anItem in $itemsList
 do
 	cp -R $deployerRoot$sourceUserDir$anItem ~/
 done
