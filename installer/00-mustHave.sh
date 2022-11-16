c0b2a7b4be67f33c54ddc54aa4fcbf355d7f9e31
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-16 17:00:13 +0300

    add some presets in user directory

--- installer/00-mustHave.sh
+++ installer/00-mustHave.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+echo update & install packets
 apt update
 apt install -y vim zsh tmux mc make
+
+
+echo add presets to $userDir
+deployerRoot=$(dirname $0)
+cp $deployerRoot ~/
 
 echo 
 echo ________________
