9f93918a5f292c3d5d7afe6ec92136f8937b1d9e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-16 10:27:34 +0300

    Update and rename makeInstallersExecutabled.sh to startConfig.sh

--- startConfig.sh
+++ startConfig.sh
@@ -4,4 +4,8 @@ installerDir="./installer"
 
 echo "rise for <$installerDir> EXECUTABLE flag"
 chmod +x $installerDir/*
+
+echo "basic config for Git"
+git config --global user.name "$1"
+git config --global user.email "$1@inbox.ru"
 echo
