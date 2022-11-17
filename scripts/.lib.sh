bedd74a4fe86327f61c6bab5d0f13cc5476367ab
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-17 16:40:12 +0300

    Update and rename utils/lib.sh to scripts/.lib.sh

---
+++ scripts/.lib.sh
@@ -0,0 +1,34 @@
+
+
+
+
+deployerRoot=$(dirname $(dirname $0))
+sourceUserDir=/resources/userSkel
+
+itemsList=$(ls -a $deployerRoot$sourceUserDir)
+
+for anItem in $itemsList
+do
+	if [ $anItem = "." ] || [ $anItem = ".." ]
+	then
+		continue
+	fi
+	
+	aFile=$deployerRoot$sourceUserDir/$anItem
+	if [ -d $aFile ]
+	then
+		echo dir: $aFile
+	elif [ -f $aFile ]
+	then
+		echo fle: $aFile
+	else
+		echo ERROR: $aFile
+		continue
+	fi
+	cp -R $aFile /etc/skel/
+done
+
+echo 
+echo ________________
+echo $0
+echo 
