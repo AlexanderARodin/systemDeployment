7c62781e4f98017962e39c69f9f5dc67b12d172b
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-17 20:45:13 +0300

    Update copyPresets.sh

--- scripts/copyPresets.sh
+++ scripts/copyPresets.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-. ./.lib.sh
+deployerRoot=$(dirname $(dirname $0))
+. deployerRoot/scripts/.lib.sh
 
 echo ..........
 test "test123"
@@ -8,7 +9,6 @@ test "test123"
 exit 0
 
 echo add presets to user directory
-deployerRoot=$(dirname $(dirname $0))
 sourceUserDir=/resources/userSkel
 
 itemsList=$(ls -a $deployerRoot$sourceUserDir)
