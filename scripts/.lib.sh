1dab80c1f32a1bcc04fbbfa8df4d09d6bd848b06
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-17 18:47:58 +0300

    Update .lib.sh

--- scripts/.lib.sh
+++ scripts/.lib.sh
@@ -1,34 +1,28 @@
 
 
-
-
-deployerRoot=$(dirname $(dirname $0))
-sourceUserDir=/resources/userSkel
-
-itemsList=$(ls -a $deployerRoot$sourceUserDir)
-
-for anItem in $itemsList
-do
-	if [ $anItem = "." ] || [ $anItem = ".." ]
-	then
-		continue
-	fi
+fucntion raaCopy {
+	resourcesDir=$(dirname $(dirname $0))/resources/$1
+	itemsList=$(ls -a $resourcesDir)
+	destinationDir=$2
 	
-	aFile=$deployerRoot$sourceUserDir/$anItem
-	if [ -d $aFile ]
-	then
-		echo dir: $aFile
-	elif [ -f $aFile ]
-	then
-		echo fle: $aFile
-	else
-		echo ERROR: $aFile
-		continue
-	fi
-	cp -R $aFile /etc/skel/
-done
-
-echo 
-echo ________________
-echo $0
-echo 
+	for anItem in $itemsList
+	do
+		if [ $anItem = "." ] || [ $anItem = ".." ]
+		then
+			continue
+		fi
+		
+		aFile=$resourcesDir/$anItem
+		if [ -d $aFile ]
+		then
+			echo dir: $aFile
+		elif [ -f $aFile ]
+		then
+			echo fle: $aFile
+		else
+			echo ERROR: $aFile
+			continue
+		fi
+		cp -R $aFile $destinationDir
+	done
+}
