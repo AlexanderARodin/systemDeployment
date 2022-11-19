97c940245a06d8758c8f40bb5912bdc9c58d5809
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-19 23:31:45 +0300

    Update installSwift.sh

--- scripts/installSwift.sh
+++ scripts/installSwift.sh
@@ -7,3 +7,22 @@ deployerRoot=$(dirname $(dirname $0))
 
 downloading
 # https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+
+
+apt-get install \
+          binutils \
+          git \
+          gnupg2 \
+          libc6-dev \
+          libcurl4-openssl-dev \
+          libedit2 \
+          libgcc-9-dev \
+          libpython3.8 \
+          libsqlite3-0 \
+          libstdc++-9-dev \
+          libxml2-dev \
+          libz3-dev \
+          pkg-config \
+          tzdata \
+          unzip \
+          zlib1g-dev
