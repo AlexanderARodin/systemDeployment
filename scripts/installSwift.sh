aaedd454a8704b7097336c0c56d3b625071ea5e6
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-22 00:11:05 +0300

    Update installSwift.sh

--- scripts/installSwift.sh
+++ scripts/installSwift.sh
@@ -3,43 +3,9 @@
 deployerRoot=$(dirname $(dirname $0))
 . $deployerRoot/scripts/.lib.sh
 
-# downloading
-swiftLink="https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz"
-swiftExtrName="/swift-5.7.1-RELEASE-ubuntu22.04/usr"
-swiftDir=/usr/swift-usr
-swiftTar=/swiftData.tar.gz
 
-mkdir $swiftDir
-wget -O 	$swiftDir$swiftTar $swiftLink
+downLoadSwift
 
-cd $swiftDir
-tar xzf 	.$swiftTar
-mv .$swiftExtrName/* ./
-if rm -d .$swiftExtrName
-then
-	if rm -d .$(dirname $swiftExtrName)
-	then
-		rm .$swiftTar
-	fi
-fi
+downLoadSwiftDepend
 
-
-
-apt-get install -y \
-			binutils \
-			git \
-			gnupg2 \
-			libc6-dev \
-			libcurl4-openssl-dev \
-			libedit2 \
-			libgcc-9-dev \
-			libpython3.8 \
-			libsqlite3-0 \
-			libstdc++-9-dev \
-			libxml2-dev \
-			libz3-dev \
-			pkg-config \
-			tzdata \
-			unzip \
-			zlib1g-dev
 
