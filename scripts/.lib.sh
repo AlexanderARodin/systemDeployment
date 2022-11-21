a3c5ca7b9d1842acb07c719cbe8f8094fd1b0f9d
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-21 23:58:04 +0300

    Update .lib.sh

--- scripts/.lib.sh
+++ scripts/.lib.sh
@@ -25,3 +25,24 @@ function raaCopy {
 		fi
 	done
 }
+
+function downLoadSwift {
+	swiftLink="https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz"
+	swiftExtrName="/swift-5.7.1-RELEASE-ubuntu22.04/usr"
+	swiftDir=/usr/swift-usr
+	swiftTar=/swiftData.tar.gz
+	
+	mkdir $swiftDir
+	wget -O 	$swiftDir$swiftTar $swiftLink
+	
+	cd $swiftDir
+	tar xzf 	.$swiftTar
+	mv .$swiftExtrName/* ./
+	if rm -d .$swiftExtrName
+	then
+		if rm -d .$(dirname $swiftExtrName)
+		then
+			rm .$swiftTar
+		fi
+	fi
+}
