534d8331b684e95965d36645145b3574f2086675
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-22 00:59:35 +0300

    Update installSwift.sh

--- scripts/installSwift.sh
+++ scripts/installSwift.sh
@@ -3,13 +3,13 @@
 deployerRoot=$(dirname $(dirname $0))
 . $deployerRoot/scripts/.lib.sh
 
-if $1 != "libs"
+if [ $1 != "libs" ]
 then
 	echo SWIIIIIIIIF
 #	downLoadSwift
 fi
 
-if $1 != "swift"
+if [ $1 != "swift" ]
 then
 	echo LIIIIIIIIIiiiiiibs
 #	downLoadSwiftDepend
