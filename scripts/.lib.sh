03f296bca843e49e4b3c74999198047b4b8d2cd4
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-23 00:50:42 +0300

    Update .lib.sh

--- scripts/.lib.sh
+++ scripts/.lib.sh
@@ -45,8 +45,8 @@ function downLoadSwift {
 			rm .$swiftTar
 		fi
 	fi
-	rm -Rd .$swiftExtrName/include/unicode
-	cp -R .$swiftExtrName/* /usr/
+	rm -Rd .$swiftDir/include/unicode
+	cp -R .$swiftDir/* /usr/
 }
 
 function downLoadSwiftDepend {
