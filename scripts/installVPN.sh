f7d9a40842497b216201468831e7fe5fc71f5bd6
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-11-24 17:20:49 +0300

    Update installVPN.sh

--- scripts/installVPN.sh
+++ scripts/installVPN.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 deployerRoot=$(dirname $(dirname $0))
-. $deployerRoot/scripts/.lib.sh
+. $deployerRoot/scripts/.libForVPN.sh
 
 if [ "$1" != "libs" ]
 then
 	downLoadSwift
-fi
-
-if [ "$1" != "swift" ]
-then
-	downLoadSwiftDepend
 fi
