28353755d8720f0bd0539a548fae9099bb973946
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-25 02:10:06 +0300

    Update raaping.sh

--- scripts/append-utils/raaping.sh
+++ scripts/append-utils/raaping.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 while true; do
-        for PREF in "*.." ".*." "..*" "...";do
-                HEADER="$PREF"
-                LINE="$PREF"
-                echo
-                for ITEM in $@; do
-                        HEADER="$HEADER\t$ITEM\t"
-                        LINE="$LINE\t$(ping -c 1 -t 1 $ITEM | grep icmp_ | cut -d ' ' -f 7)\t"
-                done
-                echo -e $HEADER
-                echo -e $LINE
-                sleep 1
-        done
+	for PREF in "*.." ".*." "..*" "...";do
+		HEADER="$PREF"
+		LINE="$PREF"
+		echo
+		for ITEM in $@; do
+			HEADER="$HEADER\t$ITEM\t"
+			LINE="$LINE\t$(ping -c 1 -t 1 $ITEM | grep icmp_ | cut -d ' ' -f 7)\t"
+		done
+		echo -e $HEADER
+		echo -e $LINE
+		sleep 1
+	done
 done
